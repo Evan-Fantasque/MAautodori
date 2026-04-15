fcb9e49fbaa026755b611f111e0502b256b55480
Evan-Fantasque <72699125+Evan-Fantasque@users.noreply.github.com>
2026-04-16 05:01:19 +1000

    feat: 更新安装配置，添加测试任务和自定义动作

---
+++ agent/testaction.py
@@ -0,0 +1,19 @@
+# autodori_agent.py (重构后的文件)
+import json
+import logging
+from maa.agent.agent_server import AgentServer
+from maa.custom_action import CustomAction
+from maa.context import Context
+from maa.controller import Controller
+
+
+@AgentServer.custom_action("TestAction")
+class TestAction(CustomAction):
+
+    def run(self, context: Context, argv: CustomAction.RunArg) -> bool:
+        
+        # 获取当前主程序连接的模拟器信息
+        ctrl = context.tasker.controller.info
+        logging.info(f"当前连接的模拟器信息: {ctrl}")
+        
+        return True
